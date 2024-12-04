bf24be29687826540854a3d26c8f5245107943e8
sushant <sushantneupane2056@gmail.com>
2024-12-04 10:11:30 +0545

    first commmit

---
+++ index.js
@@ -0,0 +1,16 @@
+const apiKey = "bc77dded58ea26a70c637a3a8342507c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+async function checkWeather(city) {
+    const response = await fetch (apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °c"
+    document.querySelector(".country").innerHTML = data.sys.country
+}
+searchBtn.addEventListener("click", ()=>{
+    checkWeather("&q=" + searchBox.value);
+})
